3eb34fa534534acdd55926a153efee95f7f11b5e
Lam Vo <lamvt@leap.vn>
2025-07-03 16:37:09 +0700

    Add footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RecentlyReleasedComponent } from '../../components/recently-released/recently-released.component';
 import { MovieSliderComponent } from '../../components/movie-slider/movie-slider.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import { MovieSliderComponent } from '../../components/movie-slider/movie-slider
     HeaderComponent,
     RecentlyReleasedComponent,
     MovieSliderComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',

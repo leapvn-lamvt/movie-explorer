20b2913d8c9eb3c56c3804ad74b80ae37c06c7e5
Lam Vo <lamvt@leap.vn>
2025-07-04 15:32:36 +0700

    feat: Init category page (#23)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'category', component: CategoryComponent }
 ];

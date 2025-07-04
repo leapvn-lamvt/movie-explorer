84b9c84efd10063766b7de05bda83f257e379559
Lam Vo <lamvt@leap.vn>
2025-07-04 18:46:11 +0700

    bug: Fix build error (#30)

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'category/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { slug: 'action' },
+      { slug: 'romance' },
+      { slug: 'horror' },
+      { slug: 'animation' },
+      { slug: 'adventure' },
+      { slug: 'comedy' },
+      { slug: 'science-fiction' },
+      { slug: 'documentary' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }

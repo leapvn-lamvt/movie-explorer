2423764f84da73453ab27e32bbc87ab2bcaaa788
Lam Vo <lamvt@leap.vn>
2025-07-04 10:28:19 +0700

    feat: Add route for search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent }
 ];

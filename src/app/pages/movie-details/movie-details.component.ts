75214d4ba508de2139fe6dbfacf28f7f507b6e3f
Lam Vo <lamvt@leap.vn>
2025-07-07 14:48:48 +0700

    feat: Add movie details page (#41)

---
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { MovieDetails, MovieService } from '../../services/movie.service';
+
+@Component({
+  selector: 'app-movie-details',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, FooterComponent],
+  templateUrl: './movie-details.component.html',
+  styleUrl: './movie-details.component.scss',
+})
+export class MovieDetailsComponent implements OnInit {
+  movie: MovieDetails | null = null;
+  loading = true;
+  error: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit() {
+    const imdbID = this.route.snapshot.paramMap.get('imdbID');
+    if (!imdbID) {
+      this.error = 'No movie ID provided.';
+      this.loading = false;
+      return;
+    }
+    this.loading = true;
+    this.movieService.getMovieById(imdbID).subscribe({
+      next: (movie) => {
+        this.movie = movie;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message || 'Failed to load movie details.';
+        this.loading = false;
+      },
+    });
+  }
+}

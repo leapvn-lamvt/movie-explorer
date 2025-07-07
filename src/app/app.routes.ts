e60293430ed9e150e1edeedcabe4852d018767fc
Lam Vo <lamvt@leap.vn>
2025-07-07 14:48:18 +0700

    feat: Add movie details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search', component: SearchComponent },
-  { path: 'category/:slug', component: CategoryComponent }
+  { path: 'category/:slug', component: CategoryComponent },
+  { path: 'movie-details/:imdbID', component: MovieDetailsComponent }
 ];
-

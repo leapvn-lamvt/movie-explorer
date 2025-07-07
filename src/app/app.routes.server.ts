bd48c65775973fbd67cd45038d01af5e641d8c1a
Lam Vo <lamvt@leap.vn>
2025-07-07 15:53:12 +0700

    feat: Navigate movie details page (#42)

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,21 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'movie-details/:imdbID',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: 'category/:slug',
-    renderMode: RenderMode.Prerender,
-    getPrerenderParams: async () => [
-      { slug: 'action' },
-      { slug: 'romance' },
-      { slug: 'horror' },
-      { slug: 'animation' },
-      { slug: 'adventure' },
-      { slug: 'comedy' },
-      { slug: 'science-fiction' },
-      { slug: 'documentary' }
-    ]
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];

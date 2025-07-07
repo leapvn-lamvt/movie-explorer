4ddb1067f82d8dd21ba3230196f9d7832f7d13be
Lam Vo <lamvt@leap.vn>
2025-07-07 17:38:16 +0700

    feat: Add animation for movie column items (#46)

--- src/app/components/movie-column/movie-column.component.ts
+++ src/app/components/movie-column/movie-column.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Movie } from '../../services/movie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-column',
@@ -13,4 +14,10 @@ export class MovieColumnComponent {
   @Input() loading: boolean = false;
   @Input() title!: string;
   @Input() movies!: Movie[];
+
+  constructor(private router: Router) {}
+
+  navigateToDetailsPage(imdbID: string) {
+    this.router.navigate(['/movie-details', imdbID]);
+  }
 }

46c0fbf6cce7535bc8f09acdfaa5809d7ceb1890
Lam Vo <lamvt@leap.vn>
2025-07-08 16:49:17 +0700

    feat: Add about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'category/:slug', component: CategoryComponent },
-  { path: 'movie-details/:imdbID', component: MovieDetailsComponent }
+  { path: 'movie-details/:imdbID', component: MovieDetailsComponent },
+  { path: 'about-us', component: AboutUsComponent }
 ];
